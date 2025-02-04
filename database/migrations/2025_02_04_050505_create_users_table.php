b8177119f02b75b388dd03073d3d526d02828164
irfan Ullah <iu.khan53@gmail.com>
2025-02-04 10:16:16 +0500

    Project Initialized and Database Structure Created

--- database/migrations/2025_02_04_050505_create_users_table.php
+++ database/migrations/2025_02_04_050505_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['user', 'supervisor', 'auditor', 'admin']);
+            $table->foreignId('group_id')->nullable()->constrained('groups');
+            $table->foreignId('client_id')->nullable()->constrained('clients');
+            $table->string('profile_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
